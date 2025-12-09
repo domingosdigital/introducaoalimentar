d906d94ea22ab07928bdd0e7a1c69a63a5a7efe1
Rachando o Bico <rachandobico23@gmail.com>
2025-12-09 14:24:31 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8383754234-b008f",
+  "appId": "1:844247292466:web:38037473c6da2c79107a2b",
+  "apiKey": "AIzaSyAZB19lYc198c7GXEdy4MxLj92hfk9o6Bc",
+  "authDomain": "studio-8383754234-b008f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "844247292466"
+};
